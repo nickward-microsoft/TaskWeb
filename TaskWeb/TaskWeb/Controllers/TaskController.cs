2f8f619b968ae4410f156eedcd134fb740d010c2
nickward-microsoft <nickward@microsoft.com>
2016-07-14 11:21:33 +1000

    Added TaskController, and associated items such as Task.cs, TaskManager.cs etc.

---
+++ TaskWeb/TaskWeb/Controllers/TaskController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TaskWeb.Models;
+
+namespace TaskWeb.Controllers
+{
+    public class TaskController : Controller
+    {
+
+        TaskManager _tm = new TaskManager();
+
+        // GET: Task
+        public async Task<ActionResult> Index()
+        {
+
+            await _tm.RefreshTasksAsync();
+            
+            return View(_tm.Tasks);
+        }
+    }
+}
