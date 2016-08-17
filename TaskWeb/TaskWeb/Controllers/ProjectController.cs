535519478f9642939c0594f55cffb9121632186f
Nick Ward <nickward@microsoft.com>
2016-08-17 16:05:05 +1000

    Added list projects and add a project

---
+++ TaskWeb/TaskWeb/Controllers/ProjectController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskWeb.Models;
+
+namespace TaskWeb.Controllers
+{
+    public class ProjectController : Controller
+    {
+        ProjectManager _projectManager = new ProjectManager();
+
+        // GET: Project
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            await _projectManager.RefreshProjectList();
+            return View(_projectManager.ProjectList);
+        }
+
+        // GET: AddTask
+        public async System.Threading.Tasks.Task<ActionResult> AddProject()
+        {
+            return PartialView("AddProject", new TaskWeb.Models.Project());
+        }
+
+        // POST: AddTask
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async System.Threading.Tasks.Task<ActionResult> AddProject(TaskWeb.Models.Project ProjectToAdd)
+        {
+            if (ModelState.IsValid)
+            {
+                await _projectManager.AddProjectAsync(ProjectToAdd);
+                return RedirectToAction("Index");
+            }
+
+            return PartialView("AddProject", ProjectToAdd);
+        }
+    }
+}
