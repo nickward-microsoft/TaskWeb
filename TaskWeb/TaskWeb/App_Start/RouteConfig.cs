65393bd1ed5350ac2a18185a03e93b08577bcc2a
nickward-microsoft <nickward@microsoft.com>
2016-08-23 11:12:18 +1000

    Changed default route to Task controller, so that Task list is the first page of the app

--- TaskWeb/TaskWeb/App_Start/RouteConfig.cs
+++ TaskWeb/TaskWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TaskWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Task", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
